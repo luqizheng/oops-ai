09ffc62aaffb8d4b85ee984d7f9de7eb1e04280d
卢骐征 <lqz.leo@gmail.com>
2026-03-19 15:27:12 +0800

    原始数据正确返回数据格式

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:3001/",
   //baseURL: '/api',
-  timeout: 30000,
+  timeout: 120000,
   headers: {
     "Content-Type": "application/json",
   },
